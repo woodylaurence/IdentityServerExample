a7ed1203f66b9172f88a4a77e050b2c4cfcff4a5
Laurence <laurence.woodward@highspeedtraining.co.uk>
2018-02-09 16:17:29 +0000

    Added Quickstart.UI of Identity and made site use SSL.

--- IdentityServerExample.IDP/Startup.cs
+++ IdentityServerExample.IDP/Startup.cs
@@ -11,6 +11,8 @@ namespace IdentityServerExample.IDP
 		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
 		{
+			services.AddMvc();
+
 			services.AddIdentityServer()
 					.AddDeveloperSigningCredential()
 					.AddTestUsers(Config.GetUsers())
@@ -30,6 +32,9 @@ namespace IdentityServerExample.IDP
 			}
 
 			app.UseIdentityServer();
+
+			app.UseStaticFiles();
+			app.UseMvcWithDefaultRoute();
 		}
 	}
 }
