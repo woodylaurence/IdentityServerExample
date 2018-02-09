6ef58481bfad6ce6e740009c8739f6d5d8ec0b56
Laurence <laurence.woodward@highspeedtraining.co.uk>
2018-02-09 15:48:34 +0000

    Initial commit of Website and IDP projects.

---
+++ IdentityServerExample.IDP/Config.cs
@@ -0,0 +1,56 @@
+﻿using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace IdentityServerExample.IDP
+{
+	public static class Config
+	{
+		public static List<TestUser> GetUsers()
+		{
+			return new List<TestUser>
+			{
+				new TestUser
+				{
+					SubjectId = Guid.NewGuid().ToString(),
+					Username = "Frank",
+					Password = "password",
+
+					Claims = new List<Claim>
+					{
+						new Claim("given_name", "Frank"),
+						new Claim("family_name", "Underwood")
+					}
+				},
+				new TestUser
+				{
+					SubjectId = Guid.NewGuid().ToString(),
+					Username = "Claire",
+					Password = "password",
+
+					Claims = new List<Claim>
+					{
+						new Claim("given_name", "Claire"),
+						new Claim("family_name", "Underwood")
+					}
+				}
+			};
+		}
+
+		public static IEnumerable<IdentityResource> GetIdentityResources()
+		{
+			return new List<IdentityResource>
+			{
+				new IdentityResources.OpenId(),
+				new IdentityResources.Profile()
+			};
+		}
+
+		public static IEnumerable<Client> GetClients()
+		{
+			return new List<Client>();
+		}
+	}
+}
--- IdentityServerExample.IDP/Startup.cs
+++ IdentityServerExample.IDP/Startup.cs
@@ -1,47 +1,35 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace IdentityServerExample.Site
+namespace IdentityServerExample.IDP
 {
 	public class Startup
 	{
-		public Startup(IConfiguration configuration)
-		{
-			Configuration = configuration;
-		}
-
-		public IConfiguration Configuration { get; }
-
 		// This method gets called by the runtime. Use this method to add services to the container.
+		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
 		{
-			services.AddMvc();
+			services.AddIdentityServer()
+					.AddDeveloperSigningCredential()
+					.AddTestUsers(Config.GetUsers())
+					.AddInMemoryIdentityResources(Config.GetIdentityResources())
+					.AddInMemoryClients(Config.GetClients());
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
 		{
+			loggerFactory.AddConsole();
+			loggerFactory.AddDebug();
+
 			if (env.IsDevelopment())
 			{
 				app.UseDeveloperExceptionPage();
-				app.UseBrowserLink();
-			}
-			else
-			{
-				app.UseExceptionHandler("/Home/Error");
 			}
 
-			app.UseStaticFiles();
-
-			app.UseMvc(routes =>
-			{
-				routes.MapRoute(
-					name: "default",
-					template: "{controller=Home}/{action=Index}/{id?}");
-			});
+			app.UseIdentityServer();
 		}
 	}
 }
